6e704c396c46045eddb885bf92f54d5e6f0c6691
el girafo <elgirafo@proton.me>
2022-11-01 06:40:34 -0300

    fix: no dependency

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -49,24 +49,6 @@ module.exports = {
           if (!defaultPluginMdx.options.rehypePlugins) defaultPluginMdx.options.rehypePlugins = []
           defaultPluginMdx.options.rehypePlugins.push(require('rehype-katex'))
           return defaultPluginMdx
-          
-          // open external links in new tab (internal links same)
-          const ExternalLink = props => {
-            if (props.href.includes('yourwebsite.com') || props.href[0] === '/') {
-              return <a href={props.href}>{props.children}</a>
-            }
-            return (
-              <a href={props.href} target="_blank" rel="noopener noreferrer">
-                {props.children}
-              </a>
-            )
-          }
-          const components = {
-            a: ExternalLink,
-          }
-          const ExampleLayout = ({children}) => (
-              <MDXProvider components={components}>{children}</MDXProvider>
-          ) 
         },
       },
     },
